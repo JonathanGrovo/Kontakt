d3365c369bc315845f0db6a87202b6a522e01057
Jonathan Grovo <jonathan.grovo@gmail.com>
2023-10-04 20:02:21 -0400

    Working pagination for contacts

--- readContacts.js
+++ readContacts.js
@@ -1,8 +1,12 @@
-// function that fetches contacts and displays them
-function fetchContacts(searchVal) {
+// global variable that keeps track of the current page
+let currentPage = 1;
+
+// function that fetches contacts for a specific page
+function fetchContactsForPage(searchVal, currentPage) {
     fetch('readContacts.php', {
         method: 'POST',
-        body: JSON.stringify({ searchVal }),
+        // current page included in the request
+        body: JSON.stringify({ searchVal, currentPage }),
         headers: {
             'Content-Type': 'application/json',
         },
@@ -12,6 +16,8 @@ function fetchContacts(searchVal) {
         if (data.success) {
             // call function to display contacts in html
             displayContacts(data.contacts);
+            // update pagination controls
+            updatePagination(data.totalPages);
         } else {
             console.error(data);
 
@@ -25,6 +31,33 @@ function fetchContacts(searchVal) {
     });
 }
 
+// function for updating pagination controls
+function updatePagination(totalPages) {
+    // get element that contains the controls
+    const paginationControls = document.getElementById('paginationControls');
+    paginationControls.innerHTML = '' // clear existing controls
+
+    for (let page = 1; page <= totalPages; page++) {
+        const pageLink = document.createElement('span');
+        pageLink.textContent = page;
+        pageLink.classList.add('page-link');
+
+        // highlight the current page
+        if (page === currentPage) {
+            pageLink.classList.add('current-page');
+        }
+
+        // add click event listeneder to fetch contacts for the clicked page
+        pageLink.addEventListener('click', () => {
+            currentPage = page;
+            const searchVal = searchInput.value.trim();
+            fetchContactsForPage(searchVal, currentPage);
+        });
+
+        paginationControls.appendChild(pageLink);
+    }
+}
+
 // function to display contacts in the html
 function displayContacts(contacts) {
     const contactList = document.getElementById('contactList');
@@ -36,10 +69,15 @@ function displayContacts(contacts) {
     contacts.forEach((contact) => {
         const contactElement = document.createElement('div');
         contactElement.innerHTML =
-        `<p>name: ${contact.firstname} ${contact.lastname}</p>
-        <p>email and phone: ${contact.email} ${contact.phonenumber}</p>
-        <button onclick="getCredentials('${contact.firstname}', '${contact.lastname}', '${contact.email}', '${contact.phonenumber}', '${contact.contact_id}');" class="edit-button";>Edit</button>
-        <button onclick="getId('${contact.contact_id}');" class='delete-contact';>Delete</button>`
+        `<span class="tableName">${contact.firstname} ${contact.lastname}</span>
+        <span class="tableEmail">${contact.email}</span>
+        <span class="tablePhone">${contact.phonenumber}</span>
+        <button onclick="getCredentials('${contact.firstname}', 
+        '${contact.lastname}', '${contact.email}', 
+        '${contact.phonenumber}', '${contact.contact_id}');" 
+        class="edit-button";><i class="fa-solid fa-pen-to-square"></i>Edit</button>
+        <button onclick="getId('${contact.contact_id}');" 
+        class='delete-contact';><i class="fa-solid fa-trash"></i>Delete</button>`
 
         // append contactElement to contactList
         contactList.appendChild(contactElement);
@@ -55,14 +93,20 @@ searchInput.addEventListener('input', function () {
     updateContacts();
 });
 
-// listen for when dom is loaded
-document.addEventListener('DOMContentLoaded', function () {
-    // initial fetch to display all contacts when page loads
-    fetchContacts('');
-})
+// // listen for when dom is loaded
+// document.addEventListener('DOMContentLoaded', function () {
+//     // initial fetch to display all contacts when page loads
+//     fetchContacts('');
+// })
 
 // update list of contacts on contact creation, update and delete operations
 function updateContacts() {
     const searchVal = searchInput.value.trim();
-    fetchContacts(searchVal);
+    fetchContactsForPage(searchVal, currentPage);
 }
+
+// listen for when dom is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // inital fetch to display all contacts on the first page
+    fetchContactsForPage('', currentPage);
+})
--- readContacts.php
+++ readContacts.php
@@ -23,8 +23,17 @@ $data = file_get_contents('php://input');
 // decode JSON data
 $jsonData = json_decode($data, true); // true as second parameter returns an associative array
 
-// the serach value is the only thing passed in
+// the search value
 $searchVal = $jsonData['searchVal'];
+
+// get current page from JSON data
+$page = isset($jsonData['currentPage']) ? $jsonData['currentPage'] : 1; // if page is not provided, default to page 1
+
+// number of records we want to display per page
+$recordsPerPage = 2;
+
+// calculate the offset (determines what records to start at)
+$offset = ($page - 1) * $recordsPerPage;
 
 // users can search depending on firstname, lastname, email or phone number
 // we conditionally include search criteria depending on if searchVal is a provided value
@@ -33,12 +42,36 @@ $sql = "SELECT * FROM contacts WHERE user='$currUser'";
 // check to see if searchVal is not empty
 if (!empty($searchVal)) {
     // add search criteria
-    $sql .= " AND (firstname LIKE '%$searchVal%' OR lastname LIKE '%$searchVal%' OR email LIKE '%$searchVal%' OR phonenumber LIKE '%$searchVal%')";
+    $sql .= " AND (firstname LIKE '%$searchVal%' 
+    OR lastname LIKE '%$searchVal%' 
+    OR email LIKE '%$searchVal%' 
+    OR phonenumber LIKE '%$searchVal%')";
 }
+
+// applying our records limit and offset to the query
+$sql .= " LIMIT $recordsPerPage OFFSET $offset";
+
+// calculate the total count of records without pagination
+$countSql = "SELECT COUNT(*) as total FROM contacts WHERE user='$currUser'";
+
+// check to see if search is not empty
+if (!empty($searchVal)) {
+    // add search criteria
+    $countSql .= " AND (firstname LIKE '%$searchVal%' 
+    OR lastname LIKE '%$searchVal%' 
+    OR email LIKE '%$searchVal%' 
+    OR phonenumber LIKE '%$searchVal%')";
+}
+
+// gets the total number of contacts related to 
+$totalCount = $conn->query($countSql)->fetch_assoc()['total'];
+
+// send the total number of pages
+$totalPages = ceil($totalCount / $recordsPerPage);
 
 $results = $conn->query($sql); // runs the query and stores it in variable
 
-// if there are contacts related to the search
+// if there are contacts related to the page
 if ($results->num_rows > 0) {
     $contacts = []; // array that will contain our contacts
 
@@ -47,11 +80,11 @@ if ($results->num_rows > 0) {
         $contacts[] = $row;
     }
 
-    // send the list of contacts as a JSON response
-    echo json_encode(["success" => true, "contacts" => $contacts]);
+    // send the list of contacts related to the page as a JSON response
+    echo json_encode(["success" => true, "contacts" => $contacts, "totalPages" => $totalPages]);
     exit();
-} else { // in the case that the user has no contacts
-    echo json_encode(["success" => false, "message" => "No contacts found"]);
+} else { // in the case that there are no contacts related to the page
+    echo json_encode(["success" => false, "message" => "No contacts found", "totalPages" => 0]);
     exit();
 }
 
